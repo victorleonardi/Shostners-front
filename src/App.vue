3c12db7dfe6f9e67f88299ec77e00970b3b66b88
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-08-16 18:09:27 -0300

    Initiate project

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Home msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Kanit';
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color:#7A7A7A;
+}
+body {
+  background-color: #E7E7E7;
+}
+</style>

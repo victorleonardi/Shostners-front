01c87d44582e9e4217e0cb7399c5f3506342e53c
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-08-21 17:49:12 -0300

    Update to use vue-router

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,17 @@
 <template>
-  <div id="app">
-    <HeaderNav />
-    <!-- <FormBoxes > -->
-    <RegisterForm />
-  </div>
+  <HeaderNav />
+  <router-view/>
 </template>
 
 <script>
-import './styles/global.css'
+import '@/styles/global.css'
 
-import HeaderNav from "./components/HeaderNav.vue";
-import RegisterForm from "./pages/RegisterForm.vue";
-
+import HeaderNav from '@/components/HeaderNav.vue'
 
 export default {
   name: 'App',
   components: {
-    HeaderNav,
-    RegisterForm
+    HeaderNav
 }
 }
 </script>

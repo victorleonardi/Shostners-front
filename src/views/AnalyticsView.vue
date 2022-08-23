911702ffebfcfd7d2f8d6e2b7d801c2d68ba9b6f
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-08-23 01:58:20 -0300

    Add AnalyticsView

---
+++ src/views/AnalyticsView.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="analytics">
+        <h1>Analytics</h1>
+        <h2>Find your shortened url's easely.</h2>
+        <DropdownSearch />
+    </div>
+</template>
+
+<script>
+import DropdownSearch from "@/pages/DropdownSearch.vue";
+
+    export default{
+    name:"AnalyticsView",
+    components: {
+      DropdownSearch
+    }
+}
+</script>
+
+<style scoped>
+    .analytics{
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      padding: 30px 3.5%
+    }
+    h1{
+      font-size: 3.5em;
+    }
+    h2{
+      font-weight: 500;
+      font-size: 2.5em;
+    }
+    body{
+      display: flex;
+      justify-content: center;
+      flex-direction: column;
+      align-items: center;
+      padding: 10% 30px 3.5% 3.5%;
+      font-size: 1.2em;
+    }
+</style>

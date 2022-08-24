77c92b7e07d572944664a815a440d4c1af8c8e92
Victor Leonardi <victorleonardireis@poli.ufrj.br>
2022-08-24 17:37:55 -0300

    Create views for out of date range
    Create views to reach if trying to redirect to invalid urls because of date.

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import AboutView from '../views/AboutView.vue'
 import AnalyticsView from '../views/AnalyticsView.vue'
+import OutOfDateView from '../views/OutOfDateView.vue'
+import EarlyOnDateView from '../views/EarlyOnDateView.vue'
 
 const routes = [
   {
@@ -18,6 +20,16 @@ const routes = [
     path: '/about',
     name: 'about',
     component: AboutView
+  },
+  {
+    path: '/outofdate/:date&:time',
+    name: 'outofdate',
+    component: OutOfDateView
+  },
+  {
+    path: '/earlyondate/:date&:time',
+    name: 'earlyondate',
+    component: EarlyOnDateView
   }
 ]
 
---
+++ src/views/EarlyOnDateView.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="out-of-date-title">
+    <h1>Ops, looks like you URL it's not activate yet</h1>
+    <div class="out-of-date-body">
+        <h2>It's sad to know it, but...</h2>
+        <h3>
+        Try to use it after {{date}} at {{time}}, as informed by your sector or the person who sent it to you.
+        </h3>
+        <h4>
+        For more information, contact the central of midia Shostners & Shostners Corporations.
+        </h4>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default{
+    name:"EarlyOnDateView",
+    components: {
+    },
+    data() {
+        console.log(this.$route.params.date)
+        return{
+            date: this.$route.params.date,
+            time: this.$route.params.time
+        }
+    }
+}
+</script>
+
+
+<style scoped>
+    .out-of-date-title{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px 3.5%
+    }
+    h1{
+        font-size: 3.5em;
+    }
+    
+    .out-of-date-body{
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        padding: 30px 3.5%
+    }
+    h2{
+        padding-top: 8%;
+        font-weight: 500;
+        font-size: 2.5em;
+    }
+    h3{
+        /* white-space: pre; */
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        margin: auto;
+        padding: 2% 30px 0% 3.5%;
+        font-size: 1.6em;
+        word-wrap: break-word;
+        word-break: break-word;
+    }
+    h4{
+        position: absolute;
+        justify-content: center;
+        flex-direction: column;
+        align-items: center;
+        top: 98vh;
+        transform:translateY(-100%);
+        margin: auto;
+    }
+</style>
